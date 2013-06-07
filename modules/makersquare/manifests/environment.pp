e372596bccca6a73818c6966170defb684a07a37
Matt Buck <mattbuck@massrelevance.com>
2013-06-07 13:52:00 -0500

    Ensure Ruby 2 is made global ruby.

--- modules/makersquare/manifests/environment.pp
+++ modules/makersquare/manifests/environment.pp
@@ -3,12 +3,17 @@ class makersquare::environment {
 
   include stdlib
 
+  # Switch to postgres
   include mysql
   mysql::db { 'mydb': }
 
   include alfred
 
   include iterm2::stable
+  
+  include osx
+  osx::recovery_message { 'If this Mac is found, please email hello@themakersquare.com.': }
+
   include zsh
   include heroku
   include sublime_text_2
@@ -21,4 +26,8 @@ class makersquare::environment {
   include hipchat
 
   include dropbox
+
+  class { 'ruby::global':
+    version => '2.0.0'
+  }  
 }
